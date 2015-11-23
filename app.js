b9da8d3c74ae22de9ed7418098198fca22c98463
Arie <aprasetyo@traveloka.com>
2015-11-23 14:10:19 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://localhost/simpajak');
+// mongoose.connect('mongodb://localhost/simpajak');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
